efc88b146fac06798e705400b6c098a9d4a7db64
Kyle Donaldson <kyle@kedonaldson.com>
2022-12-22 04:48:45 +0000

    Adsense

--- components/SEO.js
+++ components/SEO.js
@@ -1,17 +1,19 @@
 import Head from 'next/head';
 import { Favicon } from '../public/favicon.ico';
 import ReactGA from 'react-ga4';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 
 export default function SEO({ title, description }) {
   ReactGA.initialize('G-0PPVJCZWK1');
 
   useEffect(() => {
-      ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
   });
 
   return (
     <Head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3384926302230176"
+        crossorigin="anonymous"></script>
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
